d78a0487875f8b9486e5d6b3927f3702f8b93955
Luke Wiltshire <luke@automationlogic.com>
2016-11-02 14:17:26 +0000

    Made resource group an input arg

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
+if [ $# -eq 0 ]
+  then
+    echo "Usage: deploy.sh ResourceGroupName"
+fi
 # Deploy and configure Azure resources
-azure group deployment create -f deploy.json -e params.json -g LWTechTest -n TechTestDeployment
+azure group deployment create -f deploy.json -e params.json -g $1 -n TechTestDeployment
 
 # Get public IP and test load balance
-publicIP=`azure network public-ip show LWTechTest LWTTPubIP --json | jq '.ipAddress'`
+publicIP=`azure network public-ip show $1 TTPubIP --json | jq '.ipAddress'`
 responseCode=`curl -s -o /dev/null -w "%{http_code}" http://$publicIP`
 echo "Web app response code:"
 echo $responseCode
